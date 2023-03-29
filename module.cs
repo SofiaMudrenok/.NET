94641c5438b8035fcaa1f3bb21da7bd1edf7cb5a
Sofia <93678956+SofiaMudrenok@users.noreply.github.com>
2023-03-29 09:15:29 +0300

    Create module.cs

---
+++ module.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Metrics;
+
+namespace module.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class moduleController : ControllerBase
+    {
+        [HttpGet("zavd1")]
+        public async Task<ActionResult<Tuple<int, int>>> Get(int a, int b)
+        {
+
+            if (a < 0 || b < 0)
+            {
+                return BadRequest(400);
+            }
+            int area = a * b;
+
+            int perimeter = 2 * (a + b);
+
+            return Ok(Tuple.Create(area, perimeter));
+        }
+    
+
+    [HttpGet("zavd2")]
+    public async Task<ActionResult<double>> Get(float a,float x, int n)
+    {
+        if (n < 1)
+        {
+            return BadRequest(400);
+        }
+        double Sum =Math.Pow (x+a,2.0);
+        for (int i = 0; i < n; i++)
+        {
+            Sum +=  Math.Pow(a + Sum, 2.0);
+        }
+            Sum += a;
+        return Ok(Sum);
+    }
+}}
